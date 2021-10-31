b458dcd4ee9c441b7363749c2293b947546e0af1
voxdei20 <76260707+voxdei20@users.noreply.github.com>
2021-10-31 18:05:43 +0300

    18Ekim Ders

---
+++ src/app.js
@@ -0,0 +1,17 @@
+var dolarDun =9.20
+var dolarBugun = 9.40
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i = 0; i<3;i++){
+    console.log("<li>"+konutKredileri.length+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>Konut Kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */}
